c21776d4f89131518213c48bb95219552cc7845e
= <martinelias630@gmail.com>
2022-05-25 22:11:21 +0200

    handle inputs, buttons, add data object with values

--- src/Components/TipCard/CardInputs/index.tsx
+++ src/Components/TipCard/CardInputs/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { MouseEvent, ChangeEvent, useRef } from "react";
 
 // style
 import "../../../assets/scss/main.scss";
@@ -6,30 +6,99 @@ import "../../../assets/scss/main.scss";
 type Props = {};
 
 const CardInputs = (props: Props) => {
+  const billInput = useRef<HTMLInputElement>(null);
+  const peopleInput = useRef<HTMLInputElement>(null);
+  const customInput = useRef<HTMLInputElement>(null);
+
+  let data: any = {};
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    let errorMsg = document.querySelector(".msg-zero");
+    let bill = billInput.current?.value;
+    let custom = customInput.current?.value;
+    let people = peopleInput.current?.value;
+
+    // condition if people input gets 0 number, then error message will show
+    people === "0"
+      ? errorMsg?.classList.remove("display")
+      : errorMsg?.classList.add("display");
+
+    // reset tip in data object
+    if (customInput.current === e.currentTarget) {
+      data["tip"] = custom;
+    }
+
+    // setting current values to data object
+    data["bill"] = bill;
+    data["people"] = people;
+  };
+
+  const handleClick = (e: MouseEvent) => {
+    let customInputEl = document.querySelector(
+      ".btn-custom"
+    ) as HTMLInputElement;
+
+    // remove active class from old clicked buttons
+    let buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+      button.classList.remove("active");
+    });
+
+    // condition if clicked item is customInput
+    if (e.currentTarget !== customInputEl) {
+      customInputEl.value = "";
+
+      // add active class to clicked button
+      e.currentTarget.classList.add("active");
+      // removing % from button text
+      let tip = e.currentTarget.innerHTML.split("");
+      tip[e.currentTarget.innerHTML.length - 1] = "";
+      data["tip"] = tip.join("");
+    } else {
+      data["tip"] = customInput.current?.value;
+    }
+  };
+
   return (
     <div className="card-inputs">
       <section className="bill">
         <h2>Bill</h2>
-        <input type="text" placeholder="0" />
+        <input
+          type="text"
+          placeholder="0"
+          onChange={handleInput}
+          ref={billInput}
+        />
       </section>
 
       <section className="tip">
         <h2>Select Tip %</h2>
         <div className="btn-grid">
-          <button>5%</button>
-          <button>10%</button>
-          <button>15%</button>
-          <button>25%</button>
-          <button>50%</button>
-          <input className="btn-custom" placeholder="Custom" />
+          <button onClick={handleClick}>5%</button>
+          <button onClick={handleClick}>10%</button>
+          <button onClick={handleClick}>15%</button>
+          <button onClick={handleClick}>25%</button>
+          <button onClick={handleClick}>50%</button>
+          <input
+            className="btn-custom"
+            placeholder="Custom"
+            onChange={handleInput}
+            ref={customInput}
+            onClick={handleClick}
+          />
         </div>
       </section>
 
       <section className="people">
         <h2>
-          Number of People<p className="msg-zero">Can´t be zero</p>
+          Number of People<p className="msg-zero display">Can´t be zero</p>
         </h2>
-        <input type="text" placeholder="0" />
+        <input
+          type="text"
+          placeholder="0"
+          onChange={handleInput}
+          ref={peopleInput}
+        />
       </section>
     </div>
   );
