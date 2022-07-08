b14c3a79964450b3f30eaf1ddba1ab8bce2fdc0e
= <martinelias630@gmail.com>
2022-07-08 23:14:27 +0200

    bill from input sent to tipsummary

--- src/Components/TipCard/CardInputs/index.tsx
+++ src/Components/TipCard/CardInputs/index.tsx
@@ -1,64 +1,20 @@
-import React, { MouseEvent, ChangeEvent, useRef } from "react";
+import React, { ChangeEvent } from "react";
 
 // style
 import "../../../assets/scss/main.scss";
 
-type Props = {};
+type Props = {
+  bill: string;
+  onBillChange: (data: string) => void;
+};
 
 const CardInputs = (props: Props) => {
-  // refs
-  const billInput = useRef<HTMLInputElement>(null);
-  const peopleInput = useRef<HTMLInputElement>(null);
-  const customInput = useRef<HTMLInputElement>(null);
+  const { bill, onBillChange } = props;
 
-  let data: any = {};
-
-  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-    let errorMsg = document.querySelector(".msg-zero");
-    let billCurrent = billInput.current?.value;
-    let customCurrent = customInput.current?.value;
-    let peopleCurrent = peopleInput.current?.value;
-
-    // condition if people input gets 0 number, then error message will show
-    peopleCurrent === "0"
-      ? errorMsg?.classList.remove("display")
-      : errorMsg?.classList.add("display");
-
-    // reset tip in data object
-    if (customInput.current === e.currentTarget) {
-      data.tip = customCurrent;
-    }
-
-    // setting current values to data object
-    data.bill = billCurrent;
-    data.people = peopleCurrent;
+  const handleBillChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onBillChange(e.target.value);
   };
 
-  const handleClick = (e: MouseEvent) => {
-    let customInputEl = document.querySelector(
-      ".btn-custom"
-    ) as HTMLInputElement;
-
-    // remove active class from old clicked buttons
-    let buttons = document.querySelectorAll("button");
-    buttons.forEach((button) => {
-      button.classList.remove("active");
-    });
-
-    // condition if clicked item is customInput
-    if (e.currentTarget !== customInputEl) {
-      customInputEl.value = "";
-
-      // add active class to clicked button
-      e.currentTarget.classList.add("active");
-      // removing % from button text
-      let tip = e.currentTarget.innerHTML.split("");
-      tip[e.currentTarget.innerHTML.length - 1] = "";
-      data.tip = tip.join("");
-    } else {
-      data.tip = customInput.current?.value;
-    }
-  };
   return (
     <div className="card-inputs">
       <section className="bill">
@@ -66,26 +22,21 @@ const CardInputs = (props: Props) => {
         <input
           type="text"
           placeholder="0"
-          onChange={handleInput}
-          ref={billInput}
+          onChange={handleBillChange}
+          value={bill}
         />
       </section>
 
       <section className="tip">
         <h2>Select Tip %</h2>
+
         <div className="btn-grid">
-          <button onClick={handleClick}>5%</button>
-          <button onClick={handleClick}>10%</button>
-          <button onClick={handleClick}>15%</button>
-          <button onClick={handleClick}>25%</button>
-          <button onClick={handleClick}>50%</button>
-          <input
-            className="btn-custom"
-            placeholder="Custom"
-            onChange={handleInput}
-            ref={customInput}
-            onClick={handleClick}
-          />
+          <button>5%</button>
+          <button>10%</button>
+          <button>15%</button>
+          <button>25%</button>
+          <button>50%</button>
+          <input className="btn-custom" placeholder="Custom" />
         </div>
       </section>
 
@@ -93,12 +44,7 @@ const CardInputs = (props: Props) => {
         <h2>
           Number of People<p className="msg-zero display">Can´t be zero</p>
         </h2>
-        <input
-          type="text"
-          placeholder="0"
-          onChange={handleInput}
-          ref={peopleInput}
-        />
+        <input type="text" placeholder="0" />
       </section>
     </div>
   );
