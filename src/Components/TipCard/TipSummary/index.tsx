c65f993c32c3601aa946c11f9f53b2af0e1e588b
= <martinelias630@gmail.com>
2022-07-09 00:44:46 +0200

    tipsummary math

--- src/Components/TipCard/TipSummary/index.tsx
+++ src/Components/TipCard/TipSummary/index.tsx
@@ -13,6 +13,7 @@ const TipSummary = (props: Props) => {
   const { bill, tip, people } = props;
 
   let personTip = (bill * (tip / 100)) / people;
+  let personTotal = bill / people + personTip;
 
   return (
     <div className="tip-summary">
@@ -20,14 +21,25 @@ const TipSummary = (props: Props) => {
         <h2 className="tip-person-heading">
           Tip Amount<span>/ person</span>
         </h2>
-        <p className="tip-person-sum">${personTip.toFixed(2)}</p>
+        <p className="tip-person-sum">
+          $
+          {(isNaN(personTip) || personTip === Infinity ? 0 : personTip).toFixed(
+            2
+          )}
+        </p>
       </div>
 
       <div className="tip-total">
         <h2 className="tip-total-heading">
           Total<span>/ person</span>
         </h2>
-        <p className="tip-total-sum">$0.00</p>
+        <p className="tip-total-sum">
+          $
+          {(isNaN(personTotal) || personTotal === Infinity
+            ? 0
+            : personTotal
+          ).toFixed(2)}
+        </p>
       </div>
       <button className="btn-reset" disabled>
         RESET
